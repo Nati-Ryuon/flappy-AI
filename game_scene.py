3599313ed126332b5cbf78cdff1a6722cca4a52e
Nati-Gu <aries.zata@gmail.com>
2020-07-18 17:47:42 +0900

    Make RappyEnv

--- game_scene.py
+++ game_scene.py
@@ -6,6 +6,9 @@ import collision
 from wall import Wall
 from player import Player
 
+WALL_INTERVAL = 240
+GAP = 200
+
 class GameScene():
   def __init__(self, img_dict:dict, font, SCR_RECT):
     self.SCR_RECT = SCR_RECT
@@ -23,21 +26,23 @@ class GameScene():
     self.rappy.restart()
     collision.clear_wall_obj()
 
-
-  def step(self, screen:pygame.Surface):
+  def step(self):
     self.count += 1
     event = pygame.event.get() # 一度pygame.event.get()を行うと中身が消えてしまうため、eventに格納してplayerへ渡している
 
-    self._update_all(screen, event=event)
-    self._draw_all(screen)
+    self._update_all(event=event)
     
     for e in event:
       if e.type == QUIT or (e.type == KEYDOWN and e.key == K_ESCAPE):
+        self.exit()
+
+    if self.exit_flag == True:
         pygame.quit()
-        self.exit_flag = True
 
+  def render(self, screen:pygame.Surface):
+    self._draw_all(screen)
 
-  def _update_all(self, screen:pygame.Surface, event):
+  def _update_all(self, event):
     if self.rappy.is_dead() == False:
       self.rappy.update(event)
 
@@ -52,6 +57,9 @@ class GameScene():
 
 
   def _draw_all(self, screen:pygame.Surface):
+    if self.exit_flag:
+      return
+
     screen.fill((250, 130, 80)) # 画面を黒色(#000)に塗りつぶし ⇒　夕焼けに変更
 
     self.rappy.draw(screen)
@@ -71,8 +79,6 @@ class GameScene():
 
 
   def _wall_manager(self):
-    WALL_INTERVAL = 240
-    GAP = 200
     image = self.img_dict["wall"]
     if self.count % WALL_INTERVAL == 0:
       rand_y = random.uniform(0, image.get_rect().height * 2 + GAP - self.SCR_RECT.height) # 高さをランダムに
@@ -87,3 +93,33 @@ class GameScene():
 
   def get_exit_flag(self):
     return self.exit_flag
+
+  def is_rappy_dead(self):
+    return self.rappy.is_dead()
+
+  def get_score(self):
+    return self.score
+
+
+  # 最も近い隙間の中央座標に対するプレイヤーから見た相対座標を返す(実質state専用メソッド)
+  def get_nearest_gap_distance(self):
+    posx = self.SCR_RECT.width
+    i = 0
+    index = -1
+
+    for w in self.walls:
+      if w.rect.centerx > self.rappy.rect.centerx:
+        if posx > w.rect.centerx:
+          posx = w.rect.centerx
+          index = i
+      i = i + 1
+
+    if self.walls[index].rect.top < self.SCR_RECT.top:
+        gap_pos = (self.walls[index].rect.centerx, self.walls[index].rect.top - GAP / 2)
+    else:
+        gap_pos = (self.walls[index].rect.centerx, self.walls[index].rect.bottom + GAP / 2)
+
+    return (gap_pos[0] - self.rappy.get_pos()[0], gap_pos[1], self.rappy.get_pos()[1])
+    
+  def exit(self):
+    self.exit_flag = True
